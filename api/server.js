b9da7a2a888fc911d2b7b646396eeab6f4643614
Darkwolfxj <hesitation.dotdot@gmil.com>
2020-08-20 22:03:53 -0400

    everything seems to be in working order, now

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 const axiosWithAuth = require("../utils/axiosWithAuth").axiosWithAuth()
 const server = express();
 const options = {
-    origin: "https://speedpaste.netlify.app"
+    origin: "http://localhost:3000"
 }
 const CryptoJS = require("crypto-js");
 const AES = require("crypto-js/aes");
@@ -20,17 +20,18 @@ server.get("/", (req, res) => {
 server.get("/copy/:token", (req, res) => {
     db.getText(req.params.token)
         .then(text => {
-            console.log(req.params.token, text)
-            res.status(200).json({text: `${JSON.parse(text)}`})})
-        .catch(err => res.status(400).json({ text: err.message }))
+            console.log(text)
+            console.log("params:",req.params.token, "text:", JSON.parse(text.text))
+            res.status(200).json({text: `${JSON.parse(text.text)}`})})
+        .catch(err => res.status(400).json({ error: err.message }))
 })
 server.post("/pasting", (req, res) => {
     const pastedText = JSON.stringify(req.body.text);
-    console.log(pastedText)
-    const token = CryptoJS.AES.encrypt(pastedText, 'If you\'re good at something, never doit for free').toString()
+    console.log(JSON.stringify(req.body.text))
+    const token = CryptoJS.AES.encrypt(pastedText, 'If you\'re good at something, never do it for free').toString().replace("/", "")
+    console.log(token)
     db.addText(pastedText, token)
         .then(() => {
-            console.log(token)
             axiosWithAuth
                 .post("/shorten", {
                     "long_url": `https://speedpaste.netlify.app/copying/${token}`,
