a6e9a60c1ea900f8b7a5613c4656f96d4d0866e3
Darkwolfxj <hesitation.dotdot@gmil.com>
2020-08-20 20:22:13 -0400

    added token

--- api/server.js
+++ api/server.js
@@ -36,7 +36,7 @@ server.post("/pasting", (req, res) => {
                     "long_url": `https://speedpaste.netlify.app/copying/${token}`,
                     "domain": "bit.ly",
                 })
-                .then(ans => res.status(200).json({ link: ans.data.link }))
+                .then(ans => res.status(200).json({ link: ans.data.link, token }))
                 .catch(err => res.status(400).json({err}))
         })
         .catch(err => res.status(400).json(err))
